65030b5a78c22ff8889f74cfc14559e521dce17d
Oleksii-co <110523568+Oleksii-co@users.noreply.github.com>
2023-08-12 18:23:18 +0000

    Deploying to gh-pages from @ Oleksii-co/goit-react-hw-08-phonebook@6c3b926bb00bb804303b8eb596fd93521cc62dde 🚀

---
+++ static/js/983.3a2f5be4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("main",{children:(0,a.jsx)("div",{children:"HomePage"})})}}}]);
+//# sourceMappingURL=983.3a2f5be4.chunk.js.map
